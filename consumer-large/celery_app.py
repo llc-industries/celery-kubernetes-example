2ea5d02e4b1a3b779de10c30588b195877208243
Leo LE COUTEUR <llecouteur@unyc.io>
2026-09-15 10:37:00 +0200

    working on minikube

--- consumer-large/celery_app.py
+++ consumer-large/celery_app.py
@@ -5,10 +5,8 @@ import os
 from celery import Celery
 from lcs.tasks import longest_common_substr
 
-def make_celery():
-    return Celery("consumer-large",
-            backend=os.environ.get("CELERY_RESULT_BACKEND"),
-            broker=os.environ["CELERY_BROKER_URL"])
-
-if __name__ == "__main__":
-    make_celery().worker_main()
+celery_app = Celery(
+    "consumer-large",
+    backend=os.environ.get("CELERY_RESULT_BACKEND"),
+    broker=os.environ["CELERY_BROKER_URL"],
+)
